4c40180c587753832d75669917c6f84e9451e398
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-04-29 23:48:39 +0600

    loguzz implementation

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -3,6 +3,8 @@
 namespace Apiz\Http;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Loguzz\Middleware\LogMiddleware;
 
 class Request
 {
@@ -13,9 +15,18 @@ class Request
      */
     public $http;
 
-    public function __construct($base_url, $options  = [])
-    {
-        $default = ['base_uri' => $base_url, 'timeout' => 30.0];
+    public function __construct ($base_url, $options = []) {
+        $default = [ 'base_uri' => $base_url, 'timeout' => 30.0 ];
+
+        if (isset($options['__loguzz'])) {
+            $loggingOpts = $options['__loguzz'];
+
+            $handlerStack = HandlerStack::create();
+            $handlerStack->push(new LogMiddleware($loggingOpts['logger'], $loggingOpts), 'apiz');
+            $default['handler'] = $handlerStack;
+            unset($options['__loguzz']);
+        }
+
         $opts = array_merge($default, $options);
         $this->http = new Client($opts);
     }
