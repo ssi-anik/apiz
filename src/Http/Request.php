ce11b01311e300ef2d99fac528e5780627338564
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-04-30 01:32:52 +0600

    can use custom handlers

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -2,6 +2,7 @@
 
 namespace Apiz\Http;
 
+use Apiz\Exceptions\RequirementException;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use Loguzz\Middleware\LogMiddleware;
@@ -17,15 +18,29 @@ class Request
 
     public function __construct ($base_url, $options = []) {
         $default = [ 'base_uri' => $base_url, 'timeout' => 30.0 ];
+        $handlerStack = HandlerStack::create();
+
+        // insert custom handlers before
+        if (isset($options['handler'])) {
+            // handler middleware should be the only item in $options['handler']
+            // otherwise, $options['handler'][0] should contain the middleware
+            // $options['handler'][1] can contain as handler name
+            if (!is_array($options['handler'])) {
+                throw new RequirementException('Handler must be (assoc) array');
+            }
+            foreach ( $options['handler'] as $key => $handler ) {
+                $handlerStack->push($handler, 'custom-handler-' . $key);
+            }
+            unset($options['handler']);
+        }
 
         if (isset($options['__loguzz'])) {
             $loggingOpts = $options['__loguzz'];
-
-            $handlerStack = HandlerStack::create();
             $handlerStack->push(new LogMiddleware($loggingOpts['logger'], $loggingOpts), 'apiz');
-            $default['handler'] = $handlerStack;
             unset($options['__loguzz']);
         }
+
+        $options['handler'] = $handlerStack;
 
         $opts = array_merge($default, $options);
         $this->http = new Client($opts);
